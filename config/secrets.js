37cf365cd900ad3350dfa4541a78c6618efcbd0f
sachinsharma0237 <sachinsharma0237@gmail.com>
2021-05-30 01:19:17 -0700

    config changed

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
     "DB_CONFIG":"mongodb+srv://sachin:12345@mycluster.enbah.mongodb.net/instagram",
-    "CLIENT_ID":"356014848792-h4jpo0nkcvvoa18rqc1fvqsg2oqes6bs.apps.googleusercontent.com",
-    "CLIENT_PW":"477sMW_lcz5uebolv9Rvl827"
+    "CLIENT_ID":"356014848792-9i21ns1qm4stbca5tf1do7p1odoa518k.apps.googleusercontent.com",
+    "CLIENT_PW":"zqaHc0QSeyouSARC2j3FVzqo"
 }
